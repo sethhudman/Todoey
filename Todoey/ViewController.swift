95ae179fa16b5f331df5c75c16eb9142738a9a53
Seth Hudman <sethhudman@seths-mbp.hitronhub.home>
2018-01-24 12:46:02 -0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Seth Hudman on 1/24/18.
+//  Copyright © 2018 Red Hammer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
